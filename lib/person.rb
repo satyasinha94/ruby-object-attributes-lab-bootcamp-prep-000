2ec3d661b1f595f6f7265ca28a7ff3919b7bb363
beneficial-bundle-8122 <satyasinha94@gmail.com>
2018-10-02 20:18:42 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,11 @@
 class Person
 
-  def initialize(name)
+  def name=(person_name)
     @name = name
   end
 
   def name
     @name
-  end
-
-  def name=(new_name)
-    @name = new_name
   end
 
 end
