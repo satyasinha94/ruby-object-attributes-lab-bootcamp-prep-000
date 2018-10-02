3c89d5ea87a2d993e86e199dfbbd4f8e0eb6374d
beneficial-bundle-8122 <satyasinha94@gmail.com>
2018-10-02 20:22:54 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog
-  def name=(dogs_name)
-    @name = dogs_name
+  def name=(dog_name)
+    @name = dog_name
   end
 
   def name
     @name
   end
 
-  def breed=(dogs_breed)
-    @breed = dogs_breed
+  def breed=(dog_breed)
+    @breed = dog_breed
   end
 
   def breed
